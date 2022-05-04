01b1aef8108de44820695901356c9e021173ff41
A1exsandro <alexsandro.acp@gmail.com>
2022-05-03 21:21:34 -0400

    fix: stundet scaffold

--- db/migrate/20220504011953_create_students.rb
+++ db/migrate/20220504011953_create_students.rb
@@ -2,10 +2,10 @@ class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :age
+      t.integer :age
       t.string :gender
       t.string :email
-      t.integer :phone
+      t.string :phone
       t.string :photo
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_191642) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_011953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "students", force: :cascade do |t|
     t.string "name"
-    t.string "age"
+    t.integer "age"
     t.string "gender"
     t.string "email"
-    t.integer "phone"
+    t.string "phone"
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

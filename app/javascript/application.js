0a2bd9950861c7dea58f2bf6b8b9df8dfda1b973
A1exsandro <alexsandro.acp@gmail.com>
2022-05-12 01:08:23 -0400

    set dark theme as default

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,8 +20,8 @@ closeBtn.addEventListener('click', () => {
 
 //change theme
 themeToggler.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme-variables');
+    document.body.classList.toggle('light-theme-variables');
 
-    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
 });
